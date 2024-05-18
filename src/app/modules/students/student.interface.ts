878fafeb0599c451b4737188ac34aec9242ec252
junayednoman <junayednoman05@gmail.com>
2024-05-18 07:15:47 +0600

    initial setup done

---
+++ src/app/modules/students/student.interface.ts
@@ -0,0 +1,22 @@
+import { Schema, model, connect } from 'mongoose';
+
+export type Guardian = {
+  fatherName: string;
+  motherName: string;
+  fatherOccupation: string;
+  motherOccupation: string;
+  fatherContact: string;
+  motherContact: string;
+};
+export type Student = {
+  name: string;
+  email: string;
+  gender: 'male' | 'female';
+  dateOfBirth: string;
+  contactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  isActive: true | false;
+};
